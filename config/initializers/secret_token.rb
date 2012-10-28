6a17c7ba23b9aa81cf9c7f641681675bef9b21c6
Eugen Istoc <eugenistoc@gmail.com>
2012-10-27 21:35:07 -0400

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bbc3c6de4cfe7065a12477d865d0c14c7c0521fe5864f9789408ab6dca5ffac83f7c08a32ef97170140649113fa59129432f96b9897cf83df1ea8377f7a1f669'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  subject {page}
+
+  describe "Home Page" do
+    before {visit root_path}
+
+    it {should have_selector('h1', text: 'Sample App')}
+    it {should have_selector('title', content: full_title(''))}
+    it {should have_selector('title', content: '| Home')}
+  end
+
+  describe "Help Page" do
+    before {visit help_path}
+
+    it {should have_selector('h1', text: 'Help')}
+    it {should have_selector('title', content: full_title('Help'))}
+
+  end
+
+  describe "About Page" do
+    before {visit about_path}
+
+    it {should have_selector('h1', text: 'About Us')}
+    it {should have_selector('title', content: full_title('About Us'))}
+  end
+
+  describe "Contact Page" do
+    before {visit contact_path}
+
+    it {should have_selector('h1', text: 'Contact')}
+    it {should have_selector('title', content: full_title('Contact'))}
+  end
+end
